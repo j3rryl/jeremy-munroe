6a377338dfe2464d06b995fd1fec0fb7d2484392
j3rryl <jeremymunroe0@gmail.com>
2024-05-13 21:33:25 +0300

    re-order items

--- models/ProjectModel.ts
+++ models/ProjectModel.ts
@@ -20,38 +20,6 @@ export const projects: ProjectModel[] = [
       Ongoing enhancements focused on improving system functionality and user experience.`,
   },
   {
-    id: 2,
-    title: "File Management System Revitalization",
-    imageUrl:
-      "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/7a/c1/8a/7ac18a3c-07cd-6fd9-1c26-d2d0a2a97e09/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png",
-    company: "Strathmore Business School",
-    tags: ["Bootstrap", "Laravel", "MySql"],
-    url: "https://sbsdocs.strathmore.edu",
-    content: `Bootstrap, Laravel, and MySQL were utilized for the revitalization of an existing file management system for university staff.
-      The project focused on tailoring the system for efficient utilization and enhancing accessibility.`,
-  },
-  {
-    id: 3,
-    title: "Faculty Management System",
-    company: "Strathmore Business School",
-    tags: ["Bootstrap", "Laravel", "MySql"],
-    url: "https://fms.strathmore.edu",
-    imageUrl:
-      "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/7a/c1/8a/7ac18a3c-07cd-6fd9-1c26-d2d0a2a97e09/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png",
-    content: `This project involved the utilization of Bootstrap, Laravel, and MySQL for the conceptualization, design, and implementation of a comprehensive Faculty Management System.
-      The integration of diverse functionalities aimed to support seamless administrative processes.`,
-  },
-  {
-    id: 4,
-    title: "Events Management System",
-    company: "Strathmore University",
-    url: "https://ebs.strathmore.edu/",
-    tags: ["Bootstrap", "Laravel", "MySql"],
-    imageUrl:
-      "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/7a/c1/8a/7ac18a3c-07cd-6fd9-1c26-d2d0a2a97e09/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png",
-    content: `The development of an Events Management System for Strathmore University involved utilizing Bootstrap, Laravel, and MySQL.`,
-  },
-  {
     id: 5,
     title: "Mobile Application Development for 360 Beauty Scents",
     company: "Dezanna Hub",
@@ -120,5 +88,37 @@ export const projects: ProjectModel[] = [
     imageUrl:
       "https://scontent.fnbo8-1.fna.fbcdn.net/v/t39.30808-1/236565456_216350100500436_5065310756289660292_n.png?stp=dst-png_p200x200&_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-Os0VFCAZA0Q7kNvgEr-Hgl&_nc_ht=scontent.fnbo8-1.fna&oh=00_AYD-_R4640eCYI_QI9hwPGfHZf9-phkdkW4OwI8Xyx68Zg&oe=6647F3C7",
     content: `Developed using Android, MySQL, Java, and Spring Boot.`,
+  },
+  {
+    id: 2,
+    title: "File Management System Revitalization",
+    imageUrl:
+      "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/7a/c1/8a/7ac18a3c-07cd-6fd9-1c26-d2d0a2a97e09/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png",
+    company: "Strathmore Business School",
+    tags: ["Bootstrap", "Laravel", "MySql"],
+    url: "https://sbsdocs.strathmore.edu",
+    content: `Bootstrap, Laravel, and MySQL were utilized for the revitalization of an existing file management system for university staff.
+      The project focused on tailoring the system for efficient utilization and enhancing accessibility.`,
+  },
+  {
+    id: 3,
+    title: "Faculty Management System",
+    company: "Strathmore Business School",
+    tags: ["Bootstrap", "Laravel", "MySql"],
+    url: "https://fms.strathmore.edu",
+    imageUrl:
+      "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/7a/c1/8a/7ac18a3c-07cd-6fd9-1c26-d2d0a2a97e09/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png",
+    content: `This project involved the utilization of Bootstrap, Laravel, and MySQL for the conceptualization, design, and implementation of a comprehensive Faculty Management System.
+      The integration of diverse functionalities aimed to support seamless administrative processes.`,
+  },
+  {
+    id: 4,
+    title: "Events Management System",
+    company: "Strathmore University",
+    url: "https://ebs.strathmore.edu/",
+    tags: ["Bootstrap", "Laravel", "MySql"],
+    imageUrl:
+      "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/7a/c1/8a/7ac18a3c-07cd-6fd9-1c26-d2d0a2a97e09/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png",
+    content: `The development of an Events Management System for Strathmore University involved utilizing Bootstrap, Laravel, and MySQL.`,
   },
 ];
