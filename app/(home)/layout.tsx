0a972aec677435285728803bf6ca2f17b9dde33f
j3rryl <jeremymunroe0@gmail.com>
2024-05-16 16:28:35 +0300

    vercel deploy

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../layouts/header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
 import { Provider } from "./layouts/Provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <NextTopLoader color="#008080" showSpinner={false} />
         <Provider>{children}</Provider>
+        <Analytics />
       </body>
     </html>
   );
